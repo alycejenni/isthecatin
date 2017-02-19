ab67615ae966c066d43f48fcfbbe1e632a56cb85
Alice Jenny Butcher <alycejenni@gmail.com>
2017-02-19 11:57:08 +0000

    incorrect datetime formats

--- catflap/urls.py
+++ catflap/urls.py
@@ -3,11 +3,9 @@ from django.contrib import admin
 from .settings import STATIC_ROOT
 from django.views.static import serve
 from catflapsite import views as site
-import urllib.request as ur
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
     url(r"^$", site.mainpage),
-    url(r'^img/(?P<imgdata>.*)$', site.setimage),
 ]
--- catflapsite/views.py
+++ catflapsite/views.py
@@ -7,11 +7,5 @@ from datetime import datetime as dt
 def mainpage(request):
     img = Image.objects.first()
     b64img = base64.b64encode(img.imgdata)
-    timeago = dt.now() - img.timetaken.time()
+    timeago = dt.now() - img.timetaken
     return render(request, "main.html", {"imgdata": b64img, "timetaken": img.timetaken, "timeago": timeago})
-
-def setimage(request, imgdata):
-    binarydata = imgdata.decode()
-    img = Image(imgdata=binarydata, timetaken=dt.now())
-    Image.objects.all().delete()
-    img.save()
