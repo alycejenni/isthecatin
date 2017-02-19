d103abadd1b4b82de9aad0c14998e50ea64c235f
Alice Jenny Butcher <alycejenni@gmail.com>
2017-02-19 11:59:26 +0000

    localised datetime

--- catflapsite/views.py
+++ catflapsite/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from .models import Image
 import base64
 from datetime import datetime as dt
+import pytz
 
 
 def mainpage(request):
     img = Image.objects.first()
     b64img = base64.b64encode(img.imgdata)
-    timeago = dt.now() - img.timetaken
+    london = pytz.timezone("Europe/London")
+    timeago = london.localize(dt.now()) - img.timetaken
     return render(request, "main.html", {"imgdata": b64img, "timetaken": img.timetaken, "timeago": timeago})
