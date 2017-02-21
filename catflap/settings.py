a57b4bc7caf84b599f5b4a4c7229b9e7bd4ccad8
Alice Jenny <alycejenni@gmail.com>
2017-02-21 18:39:23 +0000

    added dotenv

--- catflap/settings.py
+++ catflap/settings.py
@@ -1,23 +1,18 @@
 import os
 import dj_database_url
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"]
 
 ALLOWED_HOSTS = ["isthecatin.herokuapp.com", "localhost", "www.isthecat.in"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,16 +62,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'catflap.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
-    'default': dj_database_url.config(engine="django.db.backends.postgresql_psycopg2")
+    'default': dj_database_url.config(engine = "django.db.backends.postgresql_psycopg2")
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,10 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
 LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
@@ -107,13 +91,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
