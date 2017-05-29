2757683b8aceac84eb40abfeed61fc455adc1b0a
Alice Jenny Butcher <alycejenni@gmail.com>
2017-05-29 22:04:11 +0100

    admin features

--- catflapsite/utils.py
+++ catflapsite/utils.py
@@ -8,7 +8,7 @@ import math
 
 AWS_HEADERS = {
     "Cache-Control": "public, max-age=86400"
-    }
+}
 
 INSIDE = 1
 OUTSIDE = 0
@@ -17,7 +17,7 @@ SCHRODINGER = 2
 ACCEPTED_FILES = {
     "jpg": "img",
     "mp4": "video"
-    }
+}
 
 
 class ImgUrl(object):
@@ -28,8 +28,8 @@ class ImgUrl(object):
             dt.fromtimestamp(float(re.search(".+_(\d+_\d+)[^\d]+", key.name).groups()[0].replace("_", "."))))
         self.id = base64.urlsafe_b64encode((self.filename + settings.SALT).encode())
         self.size = key.size
-        self.url = key.generate_url(expires_in = 0, query_auth = False, response_headers = AWS_HEADERS)
-        self.httpurl = key.generate_url(expires_in = 0, query_auth = False, force_http = True)
+        self.url = key.generate_url(expires_in=0, query_auth=False, response_headers=AWS_HEADERS)
+        self.httpurl = key.generate_url(expires_in=0, query_auth=False, force_http=True)
         if "-" not in key.name:
             self.direction = SCHRODINGER
         else:
@@ -76,16 +76,16 @@ class ImgUrl(object):
 
 class S3Conn(object):
     def __init__(self):
-        self.client = boto.s3.connect_to_region("eu-west-2", aws_access_key_id = settings.AWS_KEY, is_secure = False,
-                                                aws_secret_access_key = settings.AWS_SECRET,
-                                                calling_format = boto.s3.connection.OrdinaryCallingFormat())
+        self.client = boto.s3.connect_to_region("eu-west-2", aws_access_key_id=settings.AWS_KEY, is_secure=False,
+                                                aws_secret_access_key=settings.AWS_SECRET,
+                                                calling_format=boto.s3.connection.OrdinaryCallingFormat())
         self.bucket = self.client.get_bucket(settings.IMAGE_BUCKET)
 
     @property
     def raw_keys(self):
         return list(reversed(
             sorted([k for k in self.bucket.get_all_keys() if k.name.split(".")[-1] in ACCEPTED_FILES.keys()],
-                   key = lambda x: x.last_modified)))
+                   key=lambda x: x.last_modified)))
 
     @property
     def custom_keys(self):
@@ -96,16 +96,30 @@ class S3Conn(object):
         return [k for k in self.custom_keys if k.iscat]
 
     @property
+    def notcats(self):
+        return [k for k in self.custom_keys if not k.iscat]
+
+    @property
     def latest_cat(self):
         return next(k for k in self.custom_keys if k.iscat)
 
     def get_key(self, b64imgid):
         filename = decode_filename(b64imgid)
-        return self.bucket.get_key(filename, validate = False)
+        return self.bucket.get_key(filename, validate=False)
 
     def set_not_cat(self, b64imgid):
         filename = decode_filename(b64imgid)
         new_key = "not a cat/" + filename
+        self.bucket.copy_key(new_key, settings.IMAGE_BUCKET, filename)
+        self.bucket.delete_key(filename)
+
+    def delete_key(self, b64imgid):
+        filename = decode_filename(b64imgid)
+        self.bucket.delete_key(filename)
+
+    def set_cat(self, b64imgid):
+        filename = decode_filename(b64imgid)
+        new_key = filename.replace("not%20a%20cat", "")
         self.bucket.copy_key(new_key, settings.IMAGE_BUCKET, filename)
         self.bucket.delete_key(filename)
 
